709bf1e01b4598446bb0f1cd37a9926ead2e2ff2
maxgirt <115174168+maxgirt@users.noreply.github.com>
2024-01-04 16:36:54 +0100

    fixed broker posting data to database
    - Confirm grader works
    - Retrieve problems via broker
    - Broker stores submission with result in DB
    - Grader already sends submission with result to broker
    - Add getters from submission

--- database/src/main/java/service/database_controllers/SubmissionController.java
+++ database/src/main/java/service/database_controllers/SubmissionController.java
@@ -22,6 +22,7 @@ public class SubmissionController {
         submissionEntity.setProgLanguage(submission.getProgLanguage());
         submissionEntity.setResults(submission.getResults());
         // Set other fields as necessary
+        System.out.println("SubmissionEntity: " + submissionEntity);
         return submissionEntity;
     }
 
